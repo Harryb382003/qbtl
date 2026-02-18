7963377b59190a28e10e4080d537fa874699ef30
Harry <Harryb382003@users.noreply.github.com>
2026-02-18 10:58:04 -0500

    move unused routes from Web.pm & Devel.pm to ./sandbox/unused_routes.pl

--- lib/QBTL/Devel.pm
+++ lib/QBTL/Devel.pm
@@ -177,56 +177,11 @@ sub register_routes {
       return $c->render( json => $out );
     } );
 
-  $r->get(
-    '/qbt/presence_check' => sub {
-      my $c = shift;
-
-      my $qbt  = QBTL::QBT->new( {} );
-      my $list = $qbt->get_torrents_info() || [];
-
-      my ( $total, $with_hash, $name_is_hash ) = ( 0, 0, 0 );
-      my @sample;
-
-      for my $t ( @$list ) {
-        next if ref( $t ) ne 'HASH';
-        $total++;
-
-        my $h = $t->{hash} // '';
-        $with_hash++ if $h =~ /^[0-9a-fA-F]{40}$/;
-
-        my $n = $t->{name} // '';
-        if ( $n =~ /^[0-9a-fA-F]{40}$/ ) {
-          $name_is_hash++;
-          push @sample, {hash => $h, name => $n} if @sample < 5;
-        }
-      }
-
-      $c->render(
-                  json => {
-                           total           => $total,
-                           with_hash_40hex => $with_hash,
-                           name_is_hash    => $name_is_hash,
-                           sample          => \@sample,
-                  } );
-    } );
-
   $r->post(
     '/shutdown' => sub {
       my $c = shift;
       $c->render( text => "Shutting down..." );
       Mojo::IOLoop->next_tick( sub { Mojo::IOLoop->stop } );
-    } );
-
-  $r->get(
-    '/dev_info' => sub {
-      my $c = shift;
-      $c->render( json => {dev_mode => true} );
-    } );
-
-  $r->get(
-    '/debug_ping' => sub {
-      my $c = shift;
-      $c->render( text => "debug ok" );
     } );
 
   $r->post(
@@ -241,175 +196,6 @@ sub register_routes {
 
       $c->render(
                text => "rebuilt local cache: " . scalar( keys %$local_by_ih ) );
-    } );
-
-  $r->get(
-    '/ih_debug' => sub {
-      my $c = shift;
-
-      my $opts2 = {torrent_dir => "/"};
-
-      my $scan = eval { QBTL::TorrentParser::run( opts => $opts2 ) };
-      if ( $@ ) { return $c->render( json => {error => "scan: $@"} ); }
-
-      my $qbt_by_ih = eval {
-        my $qbt = QBTL::QBT->new( $opts2 );
-        my $h   = $qbt->get_torrents_infohash();
-        $h = {} if ref( $h ) ne 'HASH';
-        $h;
-      };
-      if ( $@ ) { return $c->render( json => {error => "qbt: $@"} ); }
-
-      my $parsed = eval {
-        QBTL::TorrentParser->new(
-                                  all_torrents   => $scan->{torrents},
-                                  opts           => $opts2,
-                                  qbt_loaded_tor => $qbt_by_ih, );
-      };
-      if ( $@ ) { return $c->render( json => {error => "parse: $@"} ); }
-
-      my $local_by_ih =
-          $parsed->{by_infohash} || $parsed->{infohash_map} || {};
-
-      my @local_keys = ref( $local_by_ih ) eq 'HASH' ? keys %$local_by_ih : ();
-      my @qbt_keys   = ref( $qbt_by_ih ) eq 'HASH'   ? keys %$qbt_by_ih   : ();
-
-      my $is_40hex = sub {
-        my $s = shift // '';
-        $s =~ s/^\s+|\s+$//g;
-        return ( $s =~ /^[0-9a-fA-F]{40}$/ ) ? 1 : 0;
-      };
-
-      my $count_40hex = sub {
-        my @k = @_;
-        my $n = 0;
-        for ( @k ) { $n++ if $is_40hex->( $_ ) }
-        return $n;
-      };
-
-      my @local_sample =
-          ( sort @local_keys )[ 0 .. ( @local_keys < 5 ? $#local_keys : 4 ) ];
-      my @qbt_sample =
-          ( sort @qbt_keys )[ 0 .. ( @qbt_keys < 5 ? $#qbt_keys : 4 ) ];
-
-      $c->render(
-                  json => {
-                           local_key_count   => scalar( @local_keys ),
-                           qbt_key_count     => scalar( @qbt_keys ),
-                           local_40hex_count => $count_40hex->( @local_keys ),
-                           qbt_40hex_count   => $count_40hex->( @qbt_keys ),
-                           local_sample_keys => \@local_sample,
-                           qbt_sample_keys   => \@qbt_sample,
-                  } );
-    } );
-
-  $r->get(
-    '/overlap_debug' => sub {
-      my $c = shift;
-
-      my $opts2 = {torrent_dir => "/"};
-
-      my $scan = eval { QBTL::TorrentParser::run( opts => $opts2 ) };
-      if ( $@ ) { return $c->render( json => {error => "scan: $@"} ); }
-
-      my $qbt_by_ih = eval {
-        my $qbt = QBTL::QBT->new( $opts2 );
-        my $h   = $qbt->get_torrents_infohash();
-        $h = {} if ref( $h ) ne 'HASH';
-        $h;
-      };
-      if ( $@ ) { return $c->render( json => {error => "qbt: $@"} ); }
-
-      my $parsed = eval {
-        QBTL::TorrentParser->new(
-                                  all_torrents   => $scan->{torrents},
-                                  opts           => $opts2,
-                                  qbt_loaded_tor => $qbt_by_ih, );
-      };
-      if ( $@ ) { return $c->render( json => {error => "parse: $@"} ); }
-
-      my $local_by_ih =
-          $parsed->{by_infohash} || $parsed->{infohash_map} || {};
-      my @hits;
-
-      if ( ref( $local_by_ih ) eq 'HASH' && ref( $qbt_by_ih ) eq 'HASH' ) {
-        for my $ih ( keys %$local_by_ih ) {
-          if ( exists $qbt_by_ih->{$ih} ) {
-            push @hits, $ih;
-            last if @hits >= 10;
-          }
-        }
-      }
-
-      $c->render(
-            json => {
-              local_count => (
-                ref( $local_by_ih ) eq 'HASH' ? scalar( keys %$local_by_ih ) : 0
-              ),
-              qbt_count => (
-                    ref( $qbt_by_ih ) eq 'HASH' ? scalar( keys %$qbt_by_ih ) : 0
-              ),
-              overlap_found  => scalar( @hits ),
-              overlap_sample => \@hits,
-            } );
-    } );
-
-  $r->get(
-    '/sample_local' => sub {
-      my $c = shift;
-
-      my $opts2 = {torrent_dir => "/"};
-
-      my $scan = eval { QBTL::TorrentParser::run( opts => $opts2 ) };
-      if ( $@ ) { return $c->render( json => {error => "scan: $@"} ); }
-
-      my $parsed = eval {
-        QBTL::TorrentParser->new( all_torrents => $scan->{torrents},
-                                  opts         => $opts2, );
-      };
-      if ( $@ ) { return $c->render( json => {error => "parse: $@"} ); }
-
-      my $local_by_ih =
-          $parsed->{by_infohash} || $parsed->{infohash_map} || {};
-      if ( ref( $local_by_ih ) ne 'HASH' || !keys %$local_by_ih ) {
-        return $c->render( json => {error => "no local infohash map found"} );
-      }
-
-      my @ih = sort keys %$local_by_ih;
-      my @pick;
-      for my $i ( 0 .. 19 ) {
-        last if $i > $#ih;
-        my $h    = $ih[$i];
-        my $v    = $local_by_ih->{$h};
-        my $path = ref( $v ) eq 'HASH' ? ( $v->{source_path} // '' ) : '';
-        push @pick, {infohash => $h, path => $path};
-      }
-
-      $c->render( json => {sample => \@pick} );
-    } );
-
-  $r->get(
-    '/local_entry' => sub {
-      my $c  = shift;
-      my $ih = $c->param( 'ih' ) // '';
-
-      my $opts2 = {torrent_dir => "/"};
-
-      my $scan = QBTL::Scan::run( opts => $opts2 );
-      my $parsed =
-          QBTL::TorrentParser->new( all_torrents => $scan->{torrents},
-                                    opts         => $opts2 );
-
-      my $local_by_ih =
-          $parsed->{by_infohash} || $parsed->{infohash_map} || {};
-      my $v = ( ref( $local_by_ih ) eq 'HASH' ) ? $local_by_ih->{$ih} : undef;
-
-      $c->render(
-             json => {
-               infohash => $ih,
-               ref_type => ( defined $v ? ( ref( $v ) || 'SCALAR' ) : 'undef' ),
-               dumped   => Dumper( $v ),
-             } );
     } );
 
   $r->get(
@@ -492,12 +278,6 @@ sub register_routes {
       return;
     } );
 
-  $r->post(
-    '/queue/clear' => sub {
-      my $c = shift;
-      delete $c->app->defaults->{jobs};
-      $c->render( text => "queue cleared\n" );
-    } );
   return;
 }
 
